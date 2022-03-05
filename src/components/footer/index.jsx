0fa17a13c544f64dac2e406fc26d4e6180c4b51a
fiusks <rafabarros1@hotmail.com>
2022-03-05 20:00:08 -0300

    Footer update

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,26 +1,37 @@
 import "./style.scss";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import trianglesIcon from "../../assets/images/trianglesHeader.svg";
+import linkedinIcon from "../../assets/images/linkedinIcon.svg";
+import githubIcon from "../../assets/images/githubIcon.svg";
 
 function Footer() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" expand="lg" className="layout-footer px-5">
       <Container fluid>
-        <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand as={Link} to="home">
+          <img src={trianglesIcon} alt="logo icon" id="triangles-footer" />
+        </Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="home">
+            <Nav.Link as={Link} to="home">
               Home
             </Nav.Link>
-            <Nav.Link as={NavLink} to="portfolio">
+            <Nav.Link as={Link} to="portfolio">
               Portfolio
             </Nav.Link>
-            <Nav.Link as={NavLink} to="contact">
+            <Nav.Link as={Link} to="contact">
               Contact
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
+
+        <Navbar.Brand href="https://github.com/fiusks">
+          <img src={githubIcon} alt="logo icon" />
+        </Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={linkedinIcon} alt="logo icon" />
+        </Navbar.Brand>
       </Container>
     </Navbar>
   );
