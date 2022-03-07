4121184fa3ca90dc06a2e38171ad21b49a6495a3
fiusks <rafabarros1@hotmail.com>
2022-03-07 18:51:27 -0300

    New routes (check portolio outlet)

--- src/router.jsx
+++ src/router.jsx
@@ -3,13 +3,19 @@ import Layout from "./components/layout/layout";
 import Home from "./pages/home/index";
 import Portfolio from "./pages/portfolio/index";
 import Contact from "./pages/contact/index";
+import Manage from "./pages/portfolio/Manage";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="home" element={<Home />} />
-        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="portfolio" element={<Portfolio />}>
+          <Route path="manage" element={<Manage />} />
+          <Route path="Bookmark" />
+          <Route path="Insure" />
+          <Route path="Fylo" />
+        </Route>
         <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
