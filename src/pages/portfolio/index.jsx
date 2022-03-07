52f9146641fb64881443152cc76501bc3a4dcdb1
fiusks <rafabarros1@hotmail.com>
2022-03-07 18:55:51 -0300

    Portfolio Page creation

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -1,11 +1,54 @@
 import "./style.scss";
-import { Button } from "react-bootstrap";
+import { Outlet, useLocation } from "react-router-dom";
+import PorfolioCard from "../../components/portfolio-card";
+import manageImage from "../../assets/images/manage.png";
 
 function Portfolio() {
+  const location = useLocation();
+
   return (
     <>
-      <h1>Portfolio</h1>
-      <Button>Teste</Button>
+      {location.pathname === "/portfolio" ? (
+        <>
+          <PorfolioCard img={manageImage}>
+            <h2>Manage</h2>
+            <p>
+              Esse projeto me fez criar uma landing page responsiva de acordo
+              com o design que recebi. Usei HTML5, CSS Grid e JavaScript para as
+              áreas interativas, como o slider de testimoniais.
+            </p>
+          </PorfolioCard>
+
+          <PorfolioCard img={manageImage} flexDirection="row-reverse">
+            <h2>Manage</h2>
+            <p>
+              Esse projeto me fez criar uma landing page responsiva de acordo
+              com o design que recebi. Usei HTML5, CSS Grid e JavaScript para as
+              áreas interativas, como o slider de testimoniais.
+            </p>
+          </PorfolioCard>
+
+          <PorfolioCard img={manageImage}>
+            <h2>Manage</h2>
+            <p>
+              Esse projeto me fez criar uma landing page responsiva de acordo
+              com o design que recebi. Usei HTML5, CSS Grid e JavaScript para as
+              áreas interativas, como o slider de testimoniais.
+            </p>
+          </PorfolioCard>
+
+          <PorfolioCard img={manageImage} flexDirection="row-reverse">
+            <h2>Manage</h2>
+            <p>
+              Esse projeto me fez criar uma landing page responsiva de acordo
+              com o design que recebi. Usei HTML5, CSS Grid e JavaScript para as
+              áreas interativas, como o slider de testimoniais.
+            </p>
+          </PorfolioCard>
+        </>
+      ) : (
+        <Outlet />
+      )}
     </>
   );
 }
