4687de3eb3b07cdcf2a92ad88ec575369ce46aec
fiusks <rafabarros1@hotmail.com>
2022-03-08 10:04:39 -0300

    Header correction using rows and coluns after layout

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,33 +1,35 @@
 import "./style.scss";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, Row, Col } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import trianglesIcon from "../../assets/images/trianglesHeader.svg";
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg" className="layout-header p-5 mb-5">
-      <Container fluid>
-        <div className="header-logo-container">
-          <Navbar.Brand as={NavLink} to="home">
-            <img src={trianglesIcon} alt="logo icon" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        </div>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto text-center">
-            <Nav.Link as={NavLink} to="home">
-              Home
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="portfolio">
-              Portfolio
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="contact">
-              Contact
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Row className="gx-0">
+      <Col>
+        <Navbar expand="lg" className="layout-header">
+          <div className="header-logo-container">
+            <Navbar.Brand as={NavLink} to="home">
+              <img src={trianglesIcon} alt="logo icon" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </div>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto text-center navigation-links">
+              <Nav.Link as={NavLink} to="home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="portfolio">
+                Portfolio
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Col>
+    </Row>
   );
 }
 
