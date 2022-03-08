ecc76a26c1295f94413f51055afa257788277203
fiusks <rafabarros1@hotmail.com>
2022-03-08 10:06:12 -0300

    Manage page creation

---
+++ src/pages/portfolio/manage/index.jsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+import { Row, Col } from "react-bootstrap";
+
+function Manage() {
+  return (
+    <Row>
+      <Col>Manage</Col>
+    </Row>
+  );
+}
+
+export default Manage;
