0865dcbdde2f1af75324c232f90e18cc67f2f372
fiusks <rafabarros1@hotmail.com>
2022-03-08 10:08:23 -0300

    Changing routes style to not use outlet for porfolio subpages

--- src/router.jsx
+++ src/router.jsx
@@ -1,21 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Layout from "./components/layout/layout";
 import Home from "./pages/home/index";
-import Portfolio from "./pages/portfolio/index";
+import Portfolio from "./pages/portfolio/main";
 import Contact from "./pages/contact/index";
-import Manage from "./pages/portfolio/Manage";
+import Manage from "./pages/portfolio/manage";
+import Bookmark from "./pages/portfolio/bookmark";
+import Insure from "./pages/portfolio/insure";
+import Fylo from "./pages/portfolio/fylo";
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Navigate to="home" />} />
         <Route path="home" element={<Home />} />
-        <Route path="portfolio" element={<Portfolio />}>
-          <Route path="manage" element={<Manage />} />
-          <Route path="Bookmark" />
-          <Route path="Insure" />
-          <Route path="Fylo" />
-        </Route>
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="portfolio/manage" element={<Manage />} />
+        <Route path="portfolio/bookmark" element={<Bookmark />} />
+        <Route path="portfolio/insure" element={<Insure />} />
+        <Route path="portfolio/fylo" element={<Fylo />} />
         <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
