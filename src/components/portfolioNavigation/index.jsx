c7df514d0a6e78a821e8c4051956b48524986e5a
fiusks <rafabarros1@hotmail.com>
2022-03-08 17:44:57 -0300

    Navigation component for portfolio projects

---
+++ src/components/portfolioNavigation/index.jsx
@@ -0,0 +1,69 @@
+import "./style.scss";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import arrowRight from "../../assets/images/arrowRight.svg";
+import arrowLeft from "../../assets/images/arrowLeft.svg";
+
+function PortolioNavigation() {
+  const location = useLocation();
+
+  const pages = ["manage", "bookmark", "insure", "fylo"];
+
+  useEffect(() => {
+    function navigatePages() {
+      const pageIndex = pages.indexOf(
+        location.pathname.replace("/portfolio/", "")
+      );
+      if (pageIndex === 0) {
+        setNextPage("bookmark");
+        setPreviousPage("fylo");
+        return;
+      } else if (pageIndex === 3) {
+        setNextPage("manage");
+        setPreviousPage("insure");
+        return;
+      }
+      setNextPage(pages[pageIndex + 1]);
+      setPreviousPage(pages[pageIndex - 1]);
+      return;
+    }
+    navigatePages();
+  }, []);
+
+  const [nextPage, setNextPage] = useState("");
+  const [previousPage, setPreviousPage] = useState("");
+
+  return (
+    <Container>
+      <Row className="portfolio-nav-container">
+        <Col
+          as={Link}
+          to={`/portfolio/${previousPage}`}
+          className="left-nav-container"
+        >
+          <img src={arrowLeft} alt="arrow left" />
+          <div as={Link} className="left-nav">
+            <h3>
+              {previousPage.charAt(0).toUpperCase() + previousPage.slice(1)}
+            </h3>
+            <p>Previous Section</p>
+          </div>
+        </Col>
+        <Col
+          as={Link}
+          to={`/portfolio/${nextPage}`}
+          className="right-nav-container"
+        >
+          <img src={arrowRight} alt="arrow Right" />
+          <div as={Link} className="right-nav">
+            <h3>{nextPage.charAt(0).toUpperCase() + nextPage.slice(1)}</h3>
+            <p>Next Section</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default PortolioNavigation;
