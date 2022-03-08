7a440d308f12e366cec5a2562b3985ffb1b0e02f
fiusks <rafabarros1@hotmail.com>
2022-03-08 10:06:20 -0300

    Insure page creation

---
+++ src/pages/portfolio/insure/index.jsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+import { Row, Col } from "react-bootstrap";
+
+function Insure() {
+  return (
+    <Row>
+      <Col>Insure</Col>
+    </Row>
+  );
+}
+
+export default Insure;
