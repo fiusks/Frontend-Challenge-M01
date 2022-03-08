49d875677a0589367898367a6dc5840f00b789f4
fiusks <rafabarros1@hotmail.com>
2022-03-08 10:07:23 -0300

    Layout correction of porfolio card to be more generic

--- src/components/portfolio-card/index.jsx
+++ src/components/portfolio-card/index.jsx
@@ -3,17 +3,11 @@ import { Row, Col } from "react-bootstrap";
 
 function PorfolioCard({ img, children, flexDirection }) {
   return (
-    <Row
-      className={`card-container mb-5 ${flexDirection ? flexDirection : ""}`}
-    >
-      <Col xl={6} className="card-image">
-        <img
-          src={img}
-          alt="rafael profile picture"
-          className="img-fluid about-me-img"
-        />
+    <Row className={`card-container ${flexDirection ? flexDirection : ""}`}>
+      <Col xl={5} className="card-image">
+        <img src={img} alt="rafael profile" className="img-fluid" />
       </Col>
-      <Col xl={6}>
+      <Col xl={7} className="col-card-content">
         <div className="card-content">{children}</div>
       </Col>
     </Row>
