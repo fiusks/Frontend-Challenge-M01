0a7c527d228f64ac016b9cbc4aa83648ed2b9afb
fiusks <rafabarros1@hotmail.com>
2022-03-08 10:06:39 -0300

    Bookmark  page creation

---
+++ src/pages/portfolio/bookmark/index.jsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+import { Row, Col } from "react-bootstrap";
+
+function Bookmark() {
+  return (
+    <Row>
+      <Col>Bookmark</Col>
+    </Row>
+  );
+}
+
+export default Bookmark;
