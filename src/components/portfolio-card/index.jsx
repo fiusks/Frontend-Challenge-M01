d64419d6672b3d89d422c7ba03a1e8868653b993
fiusks <rafabarros1@hotmail.com>
2022-03-08 17:55:45 -0300

    Layout correction of the portfolio card

--- src/components/portfolio-card/index.jsx
+++ src/components/portfolio-card/index.jsx
@@ -4,10 +4,10 @@ import { Row, Col } from "react-bootstrap";
 function PorfolioCard({ img, children, flexDirection }) {
   return (
     <Row className={`card-container ${flexDirection ? flexDirection : ""}`}>
-      <Col xl={5} className="card-image">
+      <Col xl={6} className="card-image">
         <img src={img} alt="rafael profile" className="img-fluid" />
       </Col>
-      <Col xl={7} className="col-card-content">
+      <Col xl={6} className="col-card-content">
         <div className="card-content">{children}</div>
       </Col>
     </Row>
