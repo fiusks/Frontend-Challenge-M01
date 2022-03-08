ba93c6298fdec3d5b7472e2c2f6b7ba41828baf5
fiusks <rafabarros1@hotmail.com>
2022-03-08 10:04:20 -0300

    Footer correction using rows and coluns after layout

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,5 @@
 import "./style.scss";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import trianglesIcon from "../../assets/images/trianglesHeader.svg";
 import linkedinIcon from "../../assets/images/linkedinIcon.svg";
@@ -7,33 +7,35 @@ import githubIcon from "../../assets/images/githubIcon.svg";
 
 function Footer() {
   return (
-    <Navbar bg="dark" expand="lg" className="layout-footer px-5">
-      <Container fluid>
-        <Navbar.Brand as={Link} to="home">
-          <img src={trianglesIcon} alt="logo icon" id="triangles-footer" />
-        </Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="home">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="portfolio">
-              Portfolio
-            </Nav.Link>
-            <Nav.Link as={Link} to="contact">
-              Contact
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+    <Row className="gx-0">
+      <Col>
+        <Navbar bg="dark" expand="lg" className="layout-footer">
+          <Navbar.Brand as={Link} to="home">
+            <img src={trianglesIcon} alt="logo icon" id="triangles-footer" />
+          </Navbar.Brand>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto navigation-links ">
+              <Nav.Link as={Link} to="home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="portfolio">
+                Portfolio
+              </Nav.Link>
+              <Nav.Link as={Link} to="contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
 
-        <Navbar.Brand href="https://github.com/fiusks">
-          <img src={githubIcon} alt="logo icon" />
-        </Navbar.Brand>
-        <Navbar.Brand href="#home">
-          <img src={linkedinIcon} alt="logo icon" />
-        </Navbar.Brand>
-      </Container>
-    </Navbar>
+          <Navbar.Brand href="https://github.com/fiusks">
+            <img src={githubIcon} alt="logo icon" />
+          </Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img src={linkedinIcon} alt="logo icon" />
+          </Navbar.Brand>
+        </Navbar>
+      </Col>
+    </Row>
   );
 }
 
