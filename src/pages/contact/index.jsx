5e7dae4fe8a0a728ce8dc8821f3717d9e6c1cd13
fiusks <rafabarros1@hotmail.com>
2022-03-08 10:06:49 -0300

    Contact  page creation

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,12 +1,39 @@
 import "./style.scss";
-import { Button } from "react-bootstrap";
+import { Button, Row, Col, Form } from "react-bootstrap";
+import ContactForm from "../../components/contactForm";
 
 function Contact() {
   return (
-    <>
-      <h1>Contact</h1>
-      <Button variant="outline-primary">Contact</Button>
-    </>
+    <Row>
+      <Col>
+        <Row className="contact-information">
+          <Col xl={6}>
+            <h2>Entre em contato</h2>
+          </Col>
+          <Col xl={6}>
+            <p>
+              Eu adoraria escutar sobre seu trabalho atual e como eu poderia
+              ajudar. Atualmente estou procurando por um cargo e aberto para
+              várias oportunidades. Minha preferência é uma oportunidade em uma
+              empresa Britânica. Mas também estou feliz em escutar sobre
+              oportunidades remotas. Sou uma pessoa trabalhadora e positiva que
+              sempre fará as tasks com um senso de propósito e atenção aos
+              detalhes. Fique livre para checar meu perfis abaixo e entrar em
+              contato utilizando o formulário.
+            </p>
+            <div></div>
+          </Col>
+        </Row>
+        <Row className="contact-form">
+          <Col xl={6}>
+            <h2>Contato</h2>
+          </Col>
+          <Col xl={6}>
+            <ContactForm />
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   );
 }
 
