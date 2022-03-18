18a7eec5dd6f4c4a4145f44de921a2e4fbebdf32
fiusks <rafabarros1@hotmail.com>
2022-03-18 00:26:06 -0300

    Loading and Suspense added

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
+import "./i18n";
 import App from "./App";
 
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h2>Loading...</h2>
+  </div>
+);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
   document.getElementById("root")
 );
